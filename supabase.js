507cbcffbe13a763fa6fcf9172b2dc75affaf4a5
SrgjanC <cvetanovski.s@outlook.com>
2026-06-02 14:59:28 +0200

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://ndwqoarezjolommjadmo.supabase.com";
+const SUPABASE_KEY = "sb_publishable_IeVTvNXrTcJMh4VgiovMtw_CXkG1TiL";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
