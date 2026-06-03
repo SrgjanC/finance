4e2ca2b5d3e29408f1f64f30e8f638c3b46aaa14
SrgjanC <cvetanovski.s@outlook.com>
2026-06-03 09:13:34 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://ndwqoarezjolommjadmo.supabase.com";
+const SUPABASE_URL = "https://ndwqoarezjolommjadmo.supabase.co";
 const SUPABASE_KEY = "sb_publishable_IeVTvNXrTcJMh4VgiovMtw_CXkG1TiL";
 
 const supabaseClient = supabase.createClient(
